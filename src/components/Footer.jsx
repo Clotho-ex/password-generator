cd13af45643f9fb49cac8654b36f7fca166f1f38
Yusufcan Var <85075180+Clotho-ex@users.noreply.github.com>
2025-01-03 23:29:32 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 bg-transparent w-full p-4 backdrop-blur-sm">
+    <footer className="fixed bottom-0 bg-transparent w-full p-2 backdrop-blur-sm">
       <p className="text-center text-slate-400">
         Checkout my GitHub Profile {"-> "}
         <a
