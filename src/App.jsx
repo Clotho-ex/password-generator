5e525b44ab3d0c9b6507d660b2658d5d22ca8927
Yusufcan Var <85075180+Clotho-ex@users.noreply.github.com>
2025-01-04 00:20:14 +0300

    Move Footer component inside main container

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function App() {
             </p>
             <PasswordGenerator />
           </article>
-        </main>
         <Footer />
+        </main>
       </ErrorBoundary>
     </>
   );
