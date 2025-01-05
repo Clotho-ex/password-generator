94ac17c344eaa9fb1cba64ec0464ef4403bcf7c9
Yusufcan Var <85075180+Clotho-ex@users.noreply.github.com>
2025-01-05 18:54:19 +0300

    Add descriptive text to the link tag for better accesibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ const Footer = () => {
   return (
     <footer className="w-full p-2 pt-6">
       <p className="text-center text-slate-400">
-        Checkout my GitHub Profile <br />
+        Checkout my GitHub Profile <br />  
         <a
           href="https://github.com/Clotho-ex"
           className="text-green-400 hover:text-green-300 transition-colors underline underline-offset-4"
           target="_blank"
           rel="noopener noreferrer">
-          Click Here
+          View Clotho on GitHub
         </a>
       </p>
     </footer>
